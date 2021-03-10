9e6e82068f49f449040f6fbdcfebaa33ac6d7888
lauraharoescoi <lauraha.escoi@gmail.com>
2021-03-10 13:50:01 +0100

    Problema 2

---
+++ tema2/tema2_problema2/src/Problema2.java
@@ -0,0 +1,47 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+import acm.graphics.GOval;
+
+import java.awt.*;
+
+public class Problema2 extends GraphicsProgram {
+
+    public int NUM_TURTLES = 6;
+    public double TIMEOUT = 10.0;
+
+    public int rollDie() {
+        RandomGenerator rgen = RandomGenerator.getInstance();
+        return rgen.nextInt(1, 6);
+    }
+
+    public GOval[] createTurtles(double size) {
+        GOval[] turtles = new GOval[NUM_TURTLES];
+        for (int i = 0; i < NUM_TURTLES; i++) {
+            turtles[i] =  new GOval(0, size * i, size, size);
+            turtles[i].setFilled(true);
+            turtles[i].setColor(Color.GREEN);
+            add(turtles[i]);
+        }
+        return turtles;
+    }
+
+    public void turtleRace(GOval[] turtles) {
+        boolean victory = false;
+        while (!victory) {
+            for (int i = 0; i < NUM_TURTLES; i++) {
+                turtles[i].move(rollDie(), 0);
+                turtles[i].pause(TIMEOUT);
+                if (turtles[i].getLocation().getX() >= (getWidth() - turtles[i].getWidth())) {
+                    victory = true;
+                    turtles[i].setColor(Color.RED);
+                }
+            }
+        }
+    }
+
+    public void run() {
+        double size = getHeight() / NUM_TURTLES;
+        GOval[] turtles = createTurtles(size);
+        turtleRace(turtles);
+    }
+}
