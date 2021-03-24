6908abb6ea2e63e97986fe6b7eaf4dc73837e4b4
lauraharoescoi <lauraha.escoi@gmail.com>
2021-03-25 00:07:28 +0100

    Problema 8

--- tema2/tema2_problema8/src/Complex.java
+++ tema2/tema2_problema8/src/Complex.java
@@ -1,13 +1,13 @@
 public class Complex {      //a + bi
-    private float a;
-    private float b;
+    private double a;
+    private double b;
 
     public Complex(float a, float b) {
         this.a = a;
         this.b = b;
     }
 
-    public float getA() {
+    public double getA() {
         return this.a;
     }
 
@@ -15,12 +15,22 @@ public class Complex {      //a + bi
         this.a = a;
     }
 
-    public float getB() {
+    public double getB() {
         return this.b;
     }
 
     public void setB(float b) {
         this.b = b;
+    }
+
+    public void polar(Complex c) {
+        this.a = Math.sqrt(c.a * c.a + c.b * c.b);      //mòdul
+        this.b = Math.atan(c.b / c.a);                  //argument
+    }
+
+    public void cartesiana(Complex c) {
+        this.a = c.a * Math.cos(c.b);
+        this.b = c.a * Math.sin(c.b);
     }
 
     public void add(Complex c) {
@@ -33,8 +43,20 @@ public class Complex {      //a + bi
         this.b -= c.b;
     }
 
-    public void mult(Complex c) {
+    public static Complex mult(Complex c1, Complex c2) {
+        Complex result = new Complex();
+        result.a = c1.a * c2.a - c1.b * c2.b;
+        result.b = c1.a * c2.b + c1.b * c2.a;
+        return result;
+    }
 
+    public void div(Complex c) {
+        this.a = (this.a * c.a + this.b * c.b) / (c.a * c.a + c.b * c.b);
+        this.b = (this.b * c.a - this.a * c.b) / (c.a * c.a + c.b * c.b);
+    }
+
+    public String toString() {
+        return "( " + a + ", " + b + ")";
     }
 
 }
---
+++ tema2/tema2_problema8/src/Problema8.java
@@ -0,0 +1,13 @@
+import acm.program.CommandLineProgram;
+
+
+public class Problema8 extends CommandLineProgram{
+
+    public void run() {
+        Complex c1 = new Complex(2, 3);
+        Complex c2 = new Complex(5, 1);
+
+        print(c1.toString());
+    }
+
+}
