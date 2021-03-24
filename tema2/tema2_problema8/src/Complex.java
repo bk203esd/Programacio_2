0151080a210cbe341a7f285c7951ed38e0836151
lauraharoescoi <lauraha.escoi@gmail.com>
2021-03-24 12:48:34 +0100

    Problema 8

--- tema2/tema2_problema8/src/Complex.java
+++ tema2/tema2_problema8/src/Complex.java
@@ -1,6 +1,40 @@
 public class Complex {      //a + bi
-    private int a;
-    private int b;
+    private float a;
+    private float b;
 
-    public Complex()
+    public Complex(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public float getA() {
+        return this.a;
+    }
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public float getB() {
+        return this.b;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    public void add(Complex c) {
+        this.a += c.a;
+        this.b += c.b;
+    }
+
+    public void sub(Complex c) {
+        this.a -= c.a;
+        this.b -= c.b;
+    }
+
+    public void mult(Complex c) {
+
+    }
+
 }
