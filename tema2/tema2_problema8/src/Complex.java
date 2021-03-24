77c0af1e99fba0becdaaf44b710493bd392e0b8b
lauraharoescoi <lauraha.escoi@gmail.com>
2021-03-25 00:39:10 +0100

    Problema 8

--- tema2/tema2_problema8/src/Complex.java
+++ tema2/tema2_problema8/src/Complex.java
@@ -2,7 +2,7 @@ public class Complex {      //a + bi
     private double a;
     private double b;
 
-    public Complex(float a, float b) {
+    public Complex(double a, double b) {
         this.a = a;
         this.b = b;
     }
@@ -33,26 +33,24 @@ public class Complex {      //a + bi
         this.b = c.a * Math.sin(c.b);
     }
 
-    public void add(Complex c) {
-        this.a += c.a;
-        this.b += c.b;
+    public static Complex add(Complex c1, Complex c2) {
+        return new Complex(c1.a + c2.a, c1.b + c2.b);
     }
 
-    public void sub(Complex c) {
-        this.a -= c.a;
-        this.b -= c.b;
+    public static Complex sub(Complex c1, Complex c2) {
+        return new Complex(c1.a - c2.a, c1.b - c2.b);
     }
 
     public static Complex mult(Complex c1, Complex c2) {
-        Complex result = new Complex();
-        result.a = c1.a * c2.a - c1.b * c2.b;
-        result.b = c1.a * c2.b + c1.b * c2.a;
-        return result;
+        double a = c1.a * c2.a - c1.b * c2.b;
+        double b = c1.a * c2.b + c1.b * c2.a;
+        return new Complex(a, b);
     }
 
-    public void div(Complex c) {
-        this.a = (this.a * c.a + this.b * c.b) / (c.a * c.a + c.b * c.b);
-        this.b = (this.b * c.a - this.a * c.b) / (c.a * c.a + c.b * c.b);
+    public static Complex div(Complex c1, Complex c2) {
+        double a = (c1.a * c2.a + c1.b * c2.b) / (c2.a * c2.a + c2.b * c2.b);
+        double b = (c1.b * c2.a - c1.a * c2.b) / (c2.a * c2.a + c2.b * c2.b);
+        return new Complex(a, b);
     }
 
     public String toString() {
--- tema2/tema2_problema8/src/Problema8.java
+++ tema2/tema2_problema8/src/Problema8.java
@@ -6,7 +6,8 @@ public class Problema8 extends CommandLineProgram{
     public void run() {
         Complex c1 = new Complex(2, 3);
         Complex c2 = new Complex(5, 1);
-
+        Complex result = Complex.div(c1, c2);
+        c1.polar(c1);
         print(c1.toString());
     }
 
