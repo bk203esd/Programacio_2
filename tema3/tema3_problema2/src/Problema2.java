e0325b4ea947dc4927e2f4b59bf6732c98c3d36c
lauraharoescoi <lauraha.escoi@gmail.com>
2021-04-28 22:26:28 +0200

    28/04/2021

---
+++ tema3/tema3_problema2/src/Problema2.java
@@ -0,0 +1,13 @@
+import acm.program.CommandLineProgram;
+import java.io.*;
+
+
+
+public class Problema2 extends CommandLineProgram {
+
+    private static String FILE_NAME = "input.txt";
+
+    public void run() {
+        FileReader input = null;
+    }
+}
