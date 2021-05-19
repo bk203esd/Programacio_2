aeb0b8bc1ad83a22330a214f053a6ba80aa3e3dd
lauraharoescoi <lauraha.escoi@gmail.com>
2021-05-19 23:42:11 +0200

    Problema 1

---
+++ tema4/tema4_problema1/src/Problema1.java
@@ -0,0 +1,17 @@
+import acm.program.CommandLineProgram;
+
+public class Problema1 extends CommandLineProgram {
+
+    public void run() {
+        int n = readInt("Introdueix un nombre enter n >= 0 : ");
+        println("El factorial de " + n + " es: " + factorial(n));
+    }
+
+    public int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
